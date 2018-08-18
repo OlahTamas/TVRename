bf86e865661b9f603c266e2a9d01b8861d28491c
CalebDiGriz <caleb@caleb.hu>
2018-08-18 10:55:17 +0200

    My first form

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,5 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm {
     private JButton button1;
+    private JPanel mainPanel;
+
+    public MainForm() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public void createForm() {
+        JFrame frame = new JFrame("TVRename");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(800, 480));
+        JButton button1 = new JButton();
+        button1.setText("Nyomj meg");
+        button1.setSize(new Dimension(128, 32));
+        frame.getContentPane().add(button1);
+        frame.setVisible(true);
+    }
+
 }
--- src/Renamer.java
+++ src/Renamer.java
@@ -1,7 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Renamer {
     String path;
 
-    public static void main() {
+    public static void main( final String[] args ) {
+        createForm();
+    }
 
+    public static void createForm() {
+        MainForm mainForm = new MainForm();
+        mainForm.createForm();
     }
 }
